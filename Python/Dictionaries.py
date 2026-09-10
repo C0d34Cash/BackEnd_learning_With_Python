c7609d9c1a1ca7b0c79134d420a64c2677825bc2
C0d34Cash <mondaldebojyoti04@gmail.com>
2026-09-10 23:12:38 +0530

    Update Dictionaries.py

--- Python/Dictionaries.py
+++ Python/Dictionaries.py
@@ -14,3 +14,21 @@ print(len(band))
 # Access items
 print(band["vocals"])
 print(band.get("Guitar"))
+
+# List all the keys
+print(band.keys())
+
+# List all values
+print(band.values())
+
+# List all key/value pairs as tuples
+print(band.items())
+
+# Verify if a key exists
+print("Guitar" in band)
+print("!" in band)
+
+# Change values
+band["vocals"] = "Coverdale"
+band.update({"bass": "JPJ"})
+print(band)
